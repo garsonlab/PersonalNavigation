e56c57cdb8853b3ba4bff1b9dbfde06c394d2945
Garson <jiacheng_liu@yeah.net>
2019-09-18 12:06:23 +0800

    add Lockstep

--- js/navigations.js
+++ js/navigations.js
@@ -146,6 +146,7 @@ var data = [
             {url: "https://github.com/antonpantev/unity-movement-ai", title:"包含寻路，回避,追寻..", display:"Unity Movement AI"},
             {url: "https://github.com/AdultLink/TexturePanner", title:"霓虹灯般图片效果", display:"TexturePanner霓虹灯"},
             {url: "https://github.com/nightyan/Shader2D", title:" 一些2D效果的Shader实现", display:"Shader2D效果"},
+            {url: "https://github.com/JiepengTan/Lockstep-Tutorial", title:"帧同步及战斗预测", display:"Lockstep"},
         ]
     },
 ]
